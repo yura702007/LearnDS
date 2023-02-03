58a2f47616076fe86a2a3671235e7c3fe8025630
yura702007 <82802568+yura702007@users.noreply.github.com>
2023-02-03 15:41:15 +0300

    function for determining the number of rows and columns of a matrix

--- linear_algebra/matrices.py
+++ linear_algebra/matrices.py
@@ -1,5 +1,20 @@
+from typing import Tuple, List
+
+Matrix = List[List[float]]
+
+
+def shape(A: Matrix) -> Tuple[int, int]:
+    """Возвращает (число строк А, число столбцов А)"""
+    num_rows = len(A)
+    num_cols = len(A[0]) if A else 0  # Число элементов в первой строке
+    return num_rows, num_cols
+
+
 def main():
-    pass
+    A = [[1, 2, 3],
+         [4, 5, 6]]
+    assert shape(A) == (2, 3)  # 2 строки, 3 столбца
+    print(shape(A))
 
 
 if __name__ == '__main__':
