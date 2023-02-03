78b8e72f19dab5cbba6ddffd8b7c79f070b0aef7
yura702007 <82802568+yura702007@users.noreply.github.com>
2023-02-03 09:19:52 +0300

    create vectors

---
+++ linear_algebra/vectors.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def main():
+    # Создание вектора
+    Vector = List[float]
+    height_weight_age = [
+        175,  # Сантиметры
+        68,  # Килограммы
+        40  # Годы
+    ]
+    grades = [
+        85,  # Экзамен 1
+        90,  # Экзамен 2
+        73,  # Экзамен 3
+        68  # Экзамен 4
+    ]
+
+
+if __name__ == '__main__':
+    main()
