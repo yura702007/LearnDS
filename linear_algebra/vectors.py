59cc706c1418e03e7098869e738d8c547ce37f2b
yura702007 <82802568+yura702007@users.noreply.github.com>
2023-02-03 09:40:24 +0300

    create function sum vectors

--- linear_algebra/vectors.py
+++ linear_algebra/vectors.py
@@ -4,6 +4,12 @@ from typing import List
 Vector = List[float]
 
 
+def add(v: Vector, w: Vector) -> Vector:
+    """Сложение векторов"""
+    assert len(v) == len(w), 'Векторы должны иметь одинаковую длину'
+    return [sum(elem) for elem in zip(v, w)]
+
+
 def main():
     # Создание вектора
     height_weight_age = [
@@ -17,6 +23,11 @@ def main():
         73,  # Экзамен 3
         68  # Экзамен 4
     ]
+
+    vector_1 = [24, 15, 42]
+    vector_2 = [12, 36, 65]
+
+    print(add(vector_1, vector_2))
 
 
 if __name__ == '__main__':
